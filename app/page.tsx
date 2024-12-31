d87d1abd2e274abf06b85d62c8edd17a3315ad0b
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2024-12-30 18:46:15 -0800

    debugging removed floating nav from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ import { FloatingNav } from "@/components/ui/FloatingNav";
 import Hero from "@/components/ui/Hero";
 import { MatrixCard } from "@/components/ui/MatrixCard";
 
-import { navItems } from "@/data";
-import { Suspense } from "react";
+// import { navItems } from "@/data";
+// import { Suspense } from "react";
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
@@ -21,15 +21,15 @@ export default function Home() {
 
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-hidden">
       <div className="max-w-7xl w-full ">
-      <Suspense fallback="loading">
-        <FloatingNav navItems={navItems} />
+   
+        {/* <FloatingNav navItems={navItems} /> */}
         <Hero />
         <Grid />
         <RecentProjects />
         <Clients />
         <MatrixCard />
         <Footer />
-    </Suspense>
+
       </div>
     </main>
   );

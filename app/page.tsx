a47538e1cbc85e231e4f53bf81274571a7a29b93
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2024-12-30 17:11:27 -0800

    added 'use client' to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-
+'use client'
 import Clients from "@/components/Clients";
 
 import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
-
-
 
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Hero from "@/components/ui/Hero";
@@ -13,33 +11,20 @@ import { MatrixCard } from "@/components/ui/MatrixCard";
 
 import { navItems } from "@/data";
 
-
-
-
-
-
 export default function Home() {
   return (
-    
-      // <TracingBeam>
+    // <TracingBeam>
 
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-hidden">
-    
-    <div className="max-w-7xl w-full ">
-      
-
-      <FloatingNav navItems={navItems}/>
-      <Hero/>
-      <Grid/>
-      <RecentProjects/>
-      <Clients/>
-     <MatrixCard/>
-     <Footer/>
-   
-    </div>
-   
-   </main>
-      // </TracingBeam>
-     
+      <div className="max-w-7xl w-full ">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <MatrixCard />
+        <Footer />
+      </div>
+    </main>
   );
 }
